70215688afd4a579dee91635b5374e05c1c642a5
mouryag <mouryag99@gmail.com>
2022-03-30 23:12:05 +0530

    favicon

--- movie_recommender/urls.py
+++ movie_recommender/urls.py
@@ -17,9 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+
+
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recommend.urls')),
-]
+    ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
